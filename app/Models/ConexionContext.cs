65a1a3db7d262c80ec5ad65556da07e7433b38f0
pazbryan32 <pazbryan32@gmail.com>
2023-03-25 01:14:52 -0600

    Agregando Conexion A DB

--- app/Models/ConexionContext.cs
+++ app/Models/ConexionContext.cs
@@ -2,12 +2,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace app.Models
 {
-    public class ConexionContext
+    public class ConexionContext : DbContext
     {
-        private IConfiguration _config;
+        private readonly IConfiguration _config;
 
+        public ConexionContext(IConfiguration _config)
+        {
+            this._config = _config;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            var conexionString = this._config.GetConnectionString("NameConnectionString");
+
+            //conect to sql server with connection string from appsettings.json
+            options.UseSqlServer(conexionString);
+        }
     }
 }
