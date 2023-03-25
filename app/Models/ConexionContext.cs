b6e45ffbd5183313d7bd13ba69c6f24ebb342d3d
pazbryan32 <pazbryan32@gmail.com>
2023-03-25 17:04:51 -0600

    agregando dbSet

--- app/Models/ConexionContext.cs
+++ app/Models/ConexionContext.cs
@@ -22,5 +22,20 @@ namespace app.Models
             //conect to sql server with connection string from appsettings.json
             options.UseSqlServer(conexionString);
         }
+
+        //Dbset para integrar data a la base de datos
+        public DbSet<Cargo> Cargos { get; set; }
+        public DbSet<Detalle_Vacuna> Detalle_Vacunas { get; set; }
+        public DbSet<Detalle_Vacunacion> Detalle_Vacunacions { get; set; }
+        public DbSet<Direccion> Direccions { get; set; }
+        public DbSet<Etnia> Etnias { get; set; }
+        public DbSet<Genero> Generos { get; set; }
+        public DbSet<Medico> Medicos { get; set; }
+        public DbSet<Paciente> Pacientes { get; set; }
+        public DbSet<Rol> Rols { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Vacuna> Vacunas { get; set; }
+        public DbSet<Vacunacion> Vacunacions { get; set; }
+
     }
 }
---
+++ app/helpers/ErrorHelperMessage.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app.helpers
+{
+    public struct ErrorHelperMessage
+    {
+        public const String campoRequired = "El Campo es obligatorio";
+        public const String campoLenght = "El campo tiene un maximo de";
+
+    }
+}
